5c1de79bb95aa1274f77be760853e10d942ce9e9
Anna <example@@gmail.com>
2020-04-30 16:12:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() { //загрузка страницы вместо DOM content loaded
+
+  $('.list-item:first').hover(function() {
+
+    $(this).toggleClass('active');
+  });
+
+
+  $('.list-item:eq(2)').on('click', function() {
+
+    $('.image:even').fadeToggle('slow');
+
+  });
+  $('.list-item:eq(4)').on('click', function() {
+    $('.image:odd').animate(
+      {
+        opacity: 'toggle',
+        height: 'toggle'
+      }, 3000
+    );
+  });
+});
+
+// document.querySelectorAll('.list-item').forEach();
+// .classList
+// .addEventListener
+// $.ajax - fetch
+//animation   element.animate
